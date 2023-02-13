be05c73051ed7d77fc4909ee80e1bc01eb224cd7
Luba Kolbasova <luba1607@gmail.com>
2023-02-13 16:43:41 +0700

    Fixed

--- Task_3.c
+++ Task_3.c
@@ -4,12 +4,12 @@ int search(int* nums, int numsSize, int target){
     high = numsSize - 1;
     while (low <= high) {
         middle = (low + high) / 2;
-        if (target < nums[middle]) 
-            high = middle - 1;
-        else if (target > nums[middle])
-            low = middle + 1;
-        else
+        if (target == nums[middle])
             return middle;
+        else if (target < nums[middle])
+            high = middle - 1;
+        else
+            low = middle + 1;
     }
     return -1;
 }
